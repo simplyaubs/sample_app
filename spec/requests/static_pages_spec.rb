bd630c77c857a7dc039f0c59464de6f6d728afc6
Aubrey Howell <simplyaubs@gmail.com>
2014-07-26 20:51:16 -0600

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,11 +18,10 @@ feature "Static pages" do
       expect(page).to have_content('Help')
     end
     scenario "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/help'
       expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
-
 
   feature "About page" do
     scenario "should have the content 'About Us'" do
@@ -30,8 +29,19 @@ feature "Static pages" do
       expect(page).to have_content('About Us')
     end
     scenario "should have the right title" do
-      visit '/static_pages/home'
+      visit '/static_pages/about'
       expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  feature "Contact page" do
+    scenario "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    scenario "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us")
     end
   end
 
